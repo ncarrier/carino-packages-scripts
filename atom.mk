2e538662fbc915fc5b3376777e97dece2cb89620
Nicolas Carrier <nicolas.carrier@parrot.com>
2015-11-15 09:19:37 +0100

    alchemy build support

---
+++ atom.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := scripts
+LOCAL_DESCRIPTION := Various utility scripts for the carino system
+LOCAL_CATEGORY_PATH := system
+
+LOCAL_COPY_FILES := \
+	config_network:bin/ \
+	fake_service:bin/ \
+	ldd:bin/ \
+	service:bin/
+
+LOCAL_CREATE_LINKS := \
+	bin/restart:service \
+	bin/stop:service
+
+include $(BUILD_CUSTOM)
+
